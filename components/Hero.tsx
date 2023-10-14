020d5163a2203d465a62623f63772c192e4aad8a
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-14 15:12:34 +0200

    Add hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React from 'react';
+import Image from 'next/image';
+
 import CustomButton from './CustomButton';
 
 const Hero = () => {
@@ -23,6 +24,18 @@ const Hero = () => {
           containerStyles='bg-primary-blue text-white rounded-full mt-10'
           handleClick={handleScroll}
         />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image
+            src='/hero.png'
+            alt='hero-car-image'
+            fill
+            className='object-contain'
+          />
+
+          <div className='hero__image-overlay' />
+        </div>
       </div>
     </div>
   );
