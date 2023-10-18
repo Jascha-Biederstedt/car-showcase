3804f343430967b93c50a8a4ad7b8d40ecafbe1f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-18 16:03:05 +0200

    Update search params when submitting search form

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,49 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import SearchManufacturer from './SearchManufacturer';
 import SearchButton from '@/components/SearchButton';
 
 const SearchBar = () => {
+  const router = useRouter();
+
   const [manufacturer, setManufacturer] = useState('');
   const [model, setModel] = useState('');
 
-  const handleSearch = () => {};
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!manufacturer && !model) {
+      return alert('Please fill in the search bar');
+    }
+
+    updateSearchParams(manufacturer.toLowerCase(), model.toLowerCase());
+  };
+
+  const updateSearchParams = (manufacturer: string, model: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    } else {
+      searchParams.delete('manufacturer');
+    }
+
+    if (model) {
+      searchParams.set('model', model);
+    } else {
+      searchParams.delete('model');
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
