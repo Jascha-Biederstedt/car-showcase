35915f9e4518fa1d2004a89d15a6022630f7103b
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-18 14:18:06 +0200

    Add generate car image url fn

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export const fetchCars = async () => {
   const headers = {
     'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
@@ -29,4 +31,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
