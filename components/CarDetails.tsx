4f73c9b3709c38ed411d563d2e02624eb5d9146b
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-18 14:55:38 +0200

    Implement showing searched car images in modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { Dialog, Transition } from '@headlessui/react';
 
 import { CarDetailsProps } from '@/types';
+import { generateCarImageUrl } from '@/utils';
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
@@ -55,7 +56,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className='flex-1 flex flex-col gap-3'>
                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
                       <Image
-                        src='/hero.png'
+                        src={generateCarImageUrl(car)}
                         alt='car model'
                         fill
                         priority
@@ -66,7 +67,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className='flex gap-3'>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src='/hero.png'
+                          src={generateCarImageUrl(car, '29')}
                           alt='car model'
                           fill
                           priority
@@ -75,16 +76,16 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src='/hero.png'
+                          src={generateCarImageUrl(car, '33')}
                           alt='car model'
                           fill
                           priority
-                          className='object-contain'
+                          className='object-contain translate-y-5'
                         />
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src='/hero.png'
+                          src={generateCarImageUrl(car, '13')}
                           alt='car model'
                           fill
                           priority
