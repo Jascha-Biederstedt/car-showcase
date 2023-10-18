a72d171d5cbcc63bcf684098bfca75a98171a462
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-18 14:02:45 +0200

    Style modal car details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -37,7 +37,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom='opacity-100'
                 leaveTo='opacity-0'
               >
-                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5'>
+                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
                   <button
                     type='button'
                     className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
@@ -102,8 +102,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className='mt-3 flex flex-wrap gap-4'>
                       {Object.entries(car).map(([key, value]) => (
                         <div className='flex justify-between gap-5 w-full text-right'>
-                          <h4>{key}</h4>
-                          <p>{value}</p>
+                          <h4 className='text-grey capitalize'>
+                            {key.split('_').join(' ')}
+                          </h4>
+                          <p className='text-black-100 font-semibold'>
+                            {value}
+                          </p>
                         </div>
                       ))}
                     </div>
