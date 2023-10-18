b2dbeb1900b66d5b8797b1df2eed0e20b3bbbbbb
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-18 15:16:53 +0200

    Add search btn comp

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 import SearchManufacturer from './SearchManufacturer';
+import SearchButton from '@/components/SearchButton';
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
@@ -15,6 +17,17 @@ const SearchBar = () => {
         <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model icon'
         />
       </div>
     </form>
